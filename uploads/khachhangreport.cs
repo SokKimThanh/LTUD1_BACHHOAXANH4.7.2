b758d92f9bc74836117c86a7565575420cb153c4
Sok Kim Thanh <thanhsk1991@gmail.com>
2023-12-14 13:13:41 +0700

    merge master

--- uploads/khachhangreport.cs
+++ uploads/khachhangreport.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LTUD1_BACHHOAXANH472 {
+namespace LTUD1_BACHHOAXANH472.uploads {
     using System;
     using System.ComponentModel;
     using CrystalDecisions.Shared;
@@ -41,7 +41,7 @@ namespace LTUD1_BACHHOAXANH472 {
         
         public override string FullResourceName {
             get {
-                return "LTUD1_BACHHOAXANH472.khachhangreport.rpt";
+                return "LTUD1_BACHHOAXANH472.uploads.khachhangreport.rpt";
             }
             set {
                 // Do nothing
