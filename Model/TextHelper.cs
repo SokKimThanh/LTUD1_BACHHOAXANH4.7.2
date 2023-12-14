2bed81edcb24eec64a6afc5ccf762419fecd71da
Sok Kim Thanh <thanhsk1991@gmail.com>
2023-12-14 15:15:17 +0700

    bắt lỗi min max số điện thoại

--- Model/TextHelper.cs
+++ Model/TextHelper.cs
@@ -10,66 +10,64 @@ namespace LTUD1_BACHHOAXANH472.Model
 {
     public static class TextHelper
     {
-        public static void HandleTextChange_DiaChi(object sender)
+        /// <summary>
+        /// Chặn nhập dữ liệu
+        /// </summary>
+        /// <param name="sender"></param>
+        public static void HandleTextChange(object sender)
         {
-            if (ErrTxt.NoSymbol_TextChanged(sender))
+            // Phát hiện nhập chữ
+            Control control = sender as Control;
+            if (control != null)
             {
-                // Phát hiện nhập chữ
-                Control control = sender as Control;
-                if (control != null)
+                // Xóa ký tự cuối cùng đã nhập
+                if (control.Text.Length > 0)
                 {
-                    // Xóa ký tự cuối cùng đã nhập
-                    if (control.Text.Length > 0)
+                    control.Text = control.Text.Remove(control.Text.Length - 1);
+                    // Đặt con trỏ về cuối chuỗi
+                    if (control is TextBox)
                     {
-                        control.Text = control.Text.Remove(control.Text.Length - 1);
-                        // Đặt con trỏ về cuối chuỗi
-                        if (control is TextBox)
-                        {
-                            ((TextBox)control).SelectionStart = control.Text.Length;
-                        }
-                        else if (control is RichTextBox)
-                        {
-                            ((RichTextBox)control).SelectionStart = control.Text.Length;
-                        }
+                        ((TextBox)control).SelectionStart = control.Text.Length;
+                    }
+                    else if (control is RichTextBox)
+                    {
+                        ((RichTextBox)control).SelectionStart = control.Text.Length;
                     }
                 }
             }
         }
-        public static void HandleTextChange_Name(object sender)
+        public static void HandleTextChange(object sender, int min, int max)
         {
-            // kiểm tra 1 trong 3 cái nào dính thì chặn luôn
-            if (ErrTxt.NoNumber_TextChange(sender) || ErrTxt.NoSpace_TextChange(sender) || ErrTxt.NoSymbol_TextChanged(sender))
+            Control control = sender as Control;
+            if (control != null)
             {
-                // Phát hiện nhập chữ
-                TextBox textBox = sender as TextBox;
-                if (textBox != null)
+                // Xóa ký tự cuối cùng đã nhập nếu vượt quá số ký tự tối đa
+                if (control.Text.Length > max)
                 {
-                    // Xóa ký tự cuối cùng đã nhập
-                    if (textBox.Text.Length > 0)
-                    {
-                        textBox.Text = textBox.Text.Remove(textBox.Text.Length - 1);
-                        // Đặt con trỏ về cuối chuỗi
-                        textBox.SelectionStart = textBox.Text.Length;
-                    }
+                    control.Text = control.Text.Remove(control.Text.Length - 1);
+                }
+                // Đặt con trỏ về cuối chuỗi
+                if (control is TextBox)
+                {
+                    ((TextBox)control).SelectionStart = control.Text.Length;
+                }
+                else if (control is RichTextBox)
+                {
+                    ((RichTextBox)control).SelectionStart = control.Text.Length;
                 }
             }
         }
+
+        /// <summary>
+        /// Số điện thoại không nhập chữ, không nhập ký tự, hoặc khoảng trắng
+        /// </summary>
+        /// <param name="sender"></param>
         public static void HandleTextChange_PhoneNumber(object sender)
         {
-            if (ErrTxt.NoText_TextChange(sender))
+            int min = 9; int max = 11;
+            if (ErrTxt.NoText_TextChange(sender) || ErrTxt.NoSymbol_TextChanged(sender) || ErrTxt.NoSpace_TextChange(sender) || ErrTxt.MinMaxValue_TextChanged(sender, min, max))
             {
-                // Phát hiện nhập chữ
-                TextBox textBox = sender as TextBox;
-                if (textBox != null)
-                {
-                    // Xóa ký tự cuối cùng đã nhập
-                    if (textBox.Text.Length > 0)
-                    {
-                        textBox.Text = textBox.Text.Remove(textBox.Text.Length - 1);
-                        // Đặt con trỏ về cuối chuỗi
-                        textBox.SelectionStart = textBox.Text.Length;
-                    }
-                }
+                HandleTextChange(sender, min, max);
             }
         }
 
@@ -77,18 +75,32 @@ namespace LTUD1_BACHHOAXANH472.Model
         {
             if (ErrTxt.NoText_TextChange(sender))
             {
-                // Phát hiện nhập chữ
-                TextBox textBox = sender as TextBox;
-                if (textBox != null)
-                {
-                    // Xóa ký tự cuối cùng đã nhập
-                    if (textBox.Text.Length > 0)
-                    {
-                        textBox.Text = textBox.Text.Remove(textBox.Text.Length - 1);
-                        // Đặt con trỏ về cuối chuỗi
-                        textBox.SelectionStart = textBox.Text.Length;
-                    }
-                }
+                HandleTextChange(sender);
+            }
+        }
+
+
+        internal static void HandleTextChange_TenChiNhanh(object sender)
+        {
+            // kiểm tra 1 trong 3 cái nào dính thì chặn luôn
+            if (ErrTxt.NoNumber_TextChange(sender) || ErrTxt.NoSpace_TextChange(sender) || ErrTxt.NoSymbol_TextChanged(sender))
+            {
+                HandleTextChange(sender);
+            }
+        }
+        public static void HandleTextChange_DiaChi(object sender)
+        {
+            if (ErrTxt.NoSymbol_TextChanged(sender))
+            {
+                HandleTextChange(sender);
+            }
+        }
+        public static void HandleTextChange_FullName(object sender)
+        {
+            // kiểm tra 1 trong 3 cái nào dính thì chặn luôn
+            if (ErrTxt.NoNumber_TextChange(sender) || ErrTxt.NoSpace_TextChange(sender) || ErrTxt.NoSymbol_TextChanged(sender))
+            {
+                HandleTextChange(sender);
             }
         }
     }
