c65027408dc0dec9aa0e4cfef784ff1a30e34d76
QuyNgo2601 <145943526+QuyNgo2601@users.noreply.github.com>
2023-12-23 01:53:06 +0700

    Them server cho report

--- uploads/SanPhamTheoNgay1.cs
+++ uploads/SanPhamTheoNgay1.cs
@@ -95,14 +95,6 @@ namespace LTUD1_BACHHOAXANH472.uploads {
                 return this.DataDefinition.ParameterFields[0];
             }
         }
-        
-        [Browsable(false)]
-        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.Shared.IParameterField Parameter_ngay {
-            get {
-                return this.DataDefinition.ParameterFields[1];
-            }
-        }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
